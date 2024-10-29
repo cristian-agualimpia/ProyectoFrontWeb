c198587747cb587b91ee712ad829c9c571633a9e
Andrés Ruge <119058759+Rujnp794@users.noreply.github.com>
2024-10-29 14:48:52 -0500

    arrendamientos actuales

--- src/app/arrendador/arrendamientos-actuales/arrendamientos-actuales.component.ts
+++ src/app/arrendador/arrendamientos-actuales/arrendamientos-actuales.component.ts
@@ -1,6 +1,12 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Propiedad } from '../../../Conexion back/models/propiedad.model';
+import { Solicitud } from '../../../Conexion back/models/solicitud.model';
+import { ArrendadorService } from '../../../Conexion back/services/arrendador.service';
+import { PropiedadService } from '../../../Conexion back/services/propiedad.service';
+import { SolicitudService } from '../../../Conexion back/services/solicitud.service';
+
 @Component({
   selector: 'app-arrendamientos-actuales',
   standalone: true,
@@ -9,5 +15,43 @@ import { RouterLink } from '@angular/router';
   styleUrl: './arrendamientos-actuales.component.css'
 })
 export class ArrendamientosActualesComponent {
+  propiedades: Propiedad[] = [];
+  propiedadesArr: Propiedad[] = [];
+  fechaLlegada: Date = new(Date);
+  fechaPartida: Date = new(Date);
+  nombreArrendador: string = "Pepito Perez";
+  nombrePropiedad: string = "Apto fantastico";
+  solicitudes: Solicitud[] = [];
+  propiedad?: Propiedad
 
+  constructor(private propiedadService: PropiedadService,
+    private solicitudService: SolicitudService,
+    private arrendadorService: ArrendadorService
+  ) { }
+
+  ngOnInit(): void {
+    this.propiedadService.getPropiedadesArrendador(1).subscribe((data: Propiedad[]) => {
+      this.propiedadesArr = data;
+      this.propiedadesArr.forEach(propiedadR => {
+        this.solicitudService.getSolicitudesByPropiedadId(propiedadR.id).subscribe((data: Solicitud[]) => {
+          this.solicitudes = data;
+          this.solicitudes.forEach(solicitud => {
+            if (!solicitud.aceptacion) {
+              this.fechaLlegada = solicitud.fechaLlegada;
+              this.fechaPartida = solicitud.fechaPartida;
+              this.propiedadService.getPropiedadEspecifica(solicitud.propiedadId).subscribe((propiedad: Propiedad) => {
+                this.propiedades.push(propiedad);
+                this.propiedad = propiedad;
+                this.arrendadorService.obtenerArrendador(propiedad.arrendadorId).subscribe((arrendador: any) => {
+                  this.nombreArrendador = arrendador.nombre;
+                });
+              });
+            }
+          });
+        });
+      });
+      
+    });
+    
+  }
 }
